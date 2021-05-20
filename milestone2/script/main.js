766cf7af98c34f0969fe94c686f9d37770cf644c
GiuliaRosignoli <giuliarosignoli@gmail.com>
2021-05-20 17:48:03 +0200

    .

---
+++ milestone2/script/main.js
@@ -0,0 +1,22 @@
+console.log('Vue', Vue);
+
+const vm = new Vue({
+    el: '#vm',
+    data: {
+        discs: [],
+    },
+
+    created(){
+     // get data
+        const dataUrl = 'http://localhost/PHP%20Dischi/milestone2/scripts-php/database.php';
+
+        axios.get(dataUrl).then(result=>{
+            console.log(result.data);
+            this.discs = result.data;
+        })
+        .catch(err =>{
+            console.log(err);
+        })
+    }
+});
+
