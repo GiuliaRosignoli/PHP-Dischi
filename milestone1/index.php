082ca8e1e0e3e081040dc4dce66ce5318ea1b0b0
GiuliaRosignoli <giuliarosignoli@gmail.com>
2021-05-20 14:13:23 +0200

    .

---
+++ milestone1/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Style -->
+    <link rel="stylesheet" href="./style/style.css">
+    <!--/ Style -->
+
+    <title>PHP Dischi</title>
+</head>
+<body>
+    <header>  <!-- Header -->
+        <div class="header-wrapper">
+            <div class="container flex">
+                <img class="logo" src="./style/img/logo.png" alt="logo">
+            </div>
+        </div>
+    </header>
+    <main>   <!-- Main -->
+        <div class="main-wrapper">
+        ciaoooooooooooooooooooooo
+        </div> <!-- general wrapper -->
+    </main>
+
+</body>
+</html>
